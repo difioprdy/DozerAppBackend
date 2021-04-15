307d1b1fe23c8e702ea2018b83d4dbeeba0341dc
difioprdy <82445993+difioprdy@users.noreply.github.com>
2021-04-15 13:13:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var app = require('express')(); // Create an instance of an Express app
 
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Dozer'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('Dozer'); // Create a table for 'Dozer' with default settings
+mobileApp.tables.add('CheckListDB');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
